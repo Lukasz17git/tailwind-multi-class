e594e24eace139e3e97abc84df78ea763607e1be
Lukasz17git <lucas.m2295@gmail.com>
2023-06-07 19:43:45 +0200

    t

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ export default tw
 console.log('testing changeset try 2')
 
 console.log('why it doesnt run publish?')
-console.log('why it doesnt run publish? 2')
+console.log('why it doesnt run publish? 3')
 
 const a = 'lets see now'
