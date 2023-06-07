96076f2d87bdc3a2232f6a271e516fb9b5d78c45
Lukasz17git <lucas.m2295@gmail.com>
2023-06-07 19:57:38 +0200

    WILL IT RUN NOW?

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,6 @@ export { twTransform } from "./twTransform";
 export { tw }
 export default tw
 
-console.log('testing changeset try 2')
 
-console.log('why it doesnt run publish?')
-console.log('why it doesnt run publish? 3')
 
-const a = 'lets see now'
+// WILL IT RUN NOW?
